78c65fbe4f6de3b96c2fccd65ccc7e6788ae20d5
goodgd <dionatha.work@gmail.com>
2025-07-14 23:18:51 -0300

    fix: carousel video

--- src/components/VideoCarousel.tsx
+++ src/components/VideoCarousel.tsx
@@ -20,6 +20,7 @@ const VideoPlayer: React.FC<{
 }> = ({ video, isActive, onVideoReady }) => {
   const [isPlaying, setIsPlaying] = useState(false)
   const [showThumbnail, setShowThumbnail] = useState(true)
+  const [isHovered, setIsHovered] = useState(false)
   const videoRef = useRef<HTMLVideoElement>(null)
 
   // Controle de reprodução otimizado
@@ -68,23 +69,27 @@ const VideoPlayer: React.FC<{
   }, [isPlaying])
 
   return (
-    <div className="relative w-full h-full bg-black rounded-lg overflow-hidden">
+    <div
+      className="relative w-full h-full bg-black rounded-lg overflow-hidden cursor-pointer transition-all duration-300 hover:shadow-2xl"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={togglePlay}
+    >
       {/* Thumbnail overlay */}
       {showThumbnail && video.thumbnailSrc && (
-        <div
-          className="absolute inset-0 z-10 cursor-pointer"
-          onClick={handleThumbnailClick}
-        >
+        <div className="absolute inset-0 z-10">
           <img
             src={video.thumbnailSrc}
             alt="thumbnail"
             className="w-full h-full object-cover"
             loading="lazy"
           />
-          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-20">
-            <svg width="60" height="60" viewBox="0 0 61 61" fill="#D9FF85">
-              <path d="M30.5 5.89868C44.3075 5.89868 55.5 17.0912 55.5 30.8987C55.5 44.7062 44.3075 55.8987 30.5 55.8987C16.6925 55.8987 5.49998 44.7062 5.49998 30.8987C5.49998 17.0912 16.6925 5.89868 30.5 5.89868ZM30.5 10.8987C25.1957 10.8987 20.1086 13.0058 16.3579 16.7565C12.6071 20.5073 10.5 25.5944 10.5 30.8987C10.5 36.203 12.6071 41.2901 16.3579 45.0408C20.1086 48.7916 25.1957 50.8987 30.5 50.8987C35.8043 50.8987 40.8914 48.7916 44.6421 45.0408C48.3929 41.2901 50.5 36.203 50.5 30.8987C50.5 25.5944 48.3929 20.5073 44.6421 16.7565C40.8914 13.0058 35.8043 10.8987 30.5 10.8987ZM22.91 22.5237C22.9653 22.0638 23.127 21.6231 23.3823 21.2366C23.6377 20.8501 23.9796 20.5284 24.381 20.2971C24.7823 20.0659 25.2321 19.9314 25.6945 19.9043C26.157 19.8772 26.6194 19.9583 27.045 20.1412L27.8575 20.4987L28.9575 21.0037L29.83 21.4262L30.805 21.9162L31.88 22.4737L33.035 23.1012L34.27 23.8012L34.8925 24.1637L36.065 24.8687L37.135 25.5387L38.545 26.4587L39.705 27.2537L40.8425 28.0737L41.05 28.2262C42.6375 29.4187 42.65 31.7987 41.0525 32.9987L40.345 33.5212L39.3625 34.2112L38.1225 35.0487L37.155 35.6737L36.08 36.3487L34.8975 37.0562C34.6925 37.1779 34.4825 37.3004 34.2675 37.4237L33.0225 38.1287L31.8575 38.7612L30.7825 39.3212L29.8075 39.8112L28.545 40.4162L27.545 40.8687L27.04 41.0887C26.6146 41.2701 26.1527 41.35 25.6911 41.3221C25.2294 41.2941 24.7806 41.1591 24.3802 40.9277C23.9797 40.6963 23.6387 40.3749 23.3839 39.9889C23.1292 39.6029 22.9678 39.1629 22.9125 38.7037L22.7775 37.4562L22.7025 36.6212L22.5975 35.1237L22.5375 33.9737L22.4925 32.7087L22.4675 31.3362V29.8887L22.4925 28.5187L22.5375 27.2537L22.5975 26.1037L22.7025 24.6062L22.88 22.7812L22.91 22.5237ZM27.62 25.9112L27.5525 27.0787L27.5 28.3912L27.47 29.8412V31.3862L27.5 32.8362L27.55 34.1487L27.62 35.3162L28.66 34.7912L29.225 34.4962L30.445 33.8412L31.77 33.0937L33.075 32.3212L33.68 31.9512L34.79 31.2512L35.765 30.6137L34.785 29.9712L33.675 29.2687L32.435 28.5187C31.7799 28.1312 31.119 27.7536 30.4525 27.3862L29.2325 26.7312L28.1275 26.1637L27.62 25.9112Z" />
-            </svg>
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 transition-opacity duration-300">
+            <div className="transform transition-transform duration-300 hover:scale-110">
+              <svg width="80" height="80" viewBox="0 0 61 61" fill="#D9FF85">
+                <path d="M30.5 5.89868C44.3075 5.89868 55.5 17.0912 55.5 30.8987C55.5 44.7062 44.3075 55.8987 30.5 55.8987C16.6925 55.8987 5.49998 44.7062 5.49998 30.8987C5.49998 17.0912 16.6925 5.89868 30.5 5.89868ZM30.5 10.8987C25.1957 10.8987 20.1086 13.0058 16.3579 16.7565C12.6071 20.5073 10.5 25.5944 10.5 30.8987C10.5 36.203 12.6071 41.2901 16.3579 45.0408C20.1086 48.7916 25.1957 50.8987 30.5 50.8987C35.8043 50.8987 40.8914 48.7916 44.6421 45.0408C48.3929 41.2901 50.5 36.203 50.5 30.8987C50.5 25.5944 48.3929 20.5073 44.6421 16.7565C40.8914 13.0058 35.8043 10.8987 30.5 10.8987ZM22.91 22.5237C22.9653 22.0638 23.127 21.6231 23.3823 21.2366C23.6377 20.8501 23.9796 20.5284 24.381 20.2971C24.7823 20.0659 25.2321 19.9314 25.6945 19.9043C26.157 19.8772 26.6194 19.9583 27.045 20.1412L27.8575 20.4987L28.9575 21.0037L29.83 21.4262L30.805 21.9162L31.88 22.4737L33.035 23.1012L34.27 23.8012L34.8925 24.1637L36.065 24.8687L37.135 25.5387L38.545 26.4587L39.705 27.2537L40.8425 28.0737L41.05 28.2262C42.6375 29.4187 42.65 31.7987 41.0525 32.9987L40.345 33.5212L39.3625 34.2112L38.1225 35.0487L37.155 35.6737L36.08 36.3487L34.8975 37.0562C34.6925 37.1779 34.4825 37.3004 34.2675 37.4237L33.0225 38.1287L31.8575 38.7612L30.7825 39.3212L29.8075 39.8112L28.545 40.4162L27.545 40.8687L27.04 41.0887C26.6146 41.2701 26.1527 41.35 25.6911 41.3221C25.2294 41.2941 24.7806 41.1591 24.3802 40.9277C23.9797 40.6963 23.6387 40.3749 23.3839 39.9889C23.1292 39.6029 22.9678 39.1629 22.9125 38.7037L22.7775 37.4562L22.7025 36.6212L22.5975 35.1237L22.5375 33.9737L22.4925 32.7087L22.4675 31.3362V29.8887L22.4925 28.5187L22.5375 27.2537L22.5975 26.1037L22.7025 24.6062L22.88 22.7812L22.91 22.5237ZM27.62 25.9112L27.5525 27.0787L27.5 28.3912L27.47 29.8412V31.3862L27.5 32.8362L27.55 34.1487L27.62 35.3162L28.66 34.7912L29.225 34.4962L30.445 33.8412L31.77 33.0937L33.075 32.3212L33.68 31.9512L34.79 31.2512L35.765 30.6137L34.785 29.9712L33.675 29.2687L32.435 28.5187C31.7799 28.1312 31.119 27.7536 30.4525 27.3862L29.2325 26.7312L28.1275 26.1637L27.62 25.9112Z" />
+              </svg>
+            </div>
           </div>
         </div>
       )}
@@ -99,29 +104,31 @@ const VideoPlayer: React.FC<{
         loop
         playsInline
         muted={!isActive}
-        preload="none"
+        preload="metadata"
       />
 
-      {/* Play/Pause overlay */}
+      {/* Play/Pause overlay - apenas quando pausado */}
       {!showThumbnail && !isPlaying && (
-        <div
-          className="absolute inset-0 flex items-center justify-center cursor-pointer bg-black bg-opacity-20"
-          onClick={togglePlay}
-        >
-          <svg width="60" height="60" viewBox="0 0 61 61" fill="#D9FF85">
-            <path d="M30.5 5.89868C44.3075 5.89868 55.5 17.0912 55.5 30.8987C55.5 44.7062 44.3075 55.8987 30.5 55.8987C16.6925 55.8987 5.49998 44.7062 5.49998 30.8987C5.49998 17.0912 16.6925 5.89868 30.5 5.89868ZM30.5 10.8987C25.1957 10.8987 20.1086 13.0058 16.3579 16.7565C12.6071 20.5073 10.5 25.5944 10.5 30.8987C10.5 36.203 12.6071 41.2901 16.3579 45.0408C20.1086 48.7916 25.1957 50.8987 30.5 50.8987C35.8043 50.8987 40.8914 48.7916 44.6421 45.0408C48.3929 41.2901 50.5 36.203 50.5 30.8987C50.5 25.5944 48.3929 20.5073 44.6421 16.7565C40.8914 13.0058 35.8043 10.8987 30.5 10.8987ZM22.91 22.5237C22.9653 22.0638 23.127 21.6231 23.3823 21.2366C23.6377 20.8501 23.9796 20.5284 24.381 20.2971C24.7823 20.0659 25.2321 19.9314 25.6945 19.9043C26.157 19.8772 26.6194 19.9583 27.045 20.1412L27.8575 20.4987L28.9575 21.0037L29.83 21.4262L30.805 21.9162L31.88 22.4737L33.035 23.1012L34.27 23.8012L34.8925 24.1637L36.065 24.8687L37.135 25.5387L38.545 26.4587L39.705 27.2537L40.8425 28.0737L41.05 28.2262C42.6375 29.4187 42.65 31.7987 41.0525 32.9987L40.345 33.5212L39.3625 34.2112L38.1225 35.0487L37.155 35.6737L36.08 36.3487L34.8975 37.0562C34.6925 37.1779 34.4825 37.3004 34.2675 37.4237L33.0225 38.1287L31.8575 38.7612L30.7825 39.3212L29.8075 39.8112L28.545 40.4162L27.545 40.8687L27.04 41.0887C26.6146 41.2701 26.1527 41.35 25.6911 41.3221C25.2294 41.2941 24.7806 41.1591 24.3802 40.9277C23.9797 40.6963 23.6387 40.3749 23.3839 39.9889C23.1292 39.6029 22.9678 39.1629 22.9125 38.7037L22.7775 37.4562L22.7025 36.6212L22.5975 35.1237L22.5375 33.9737L22.4925 32.7087L22.4675 31.3362V29.8887L22.4925 28.5187L22.5375 27.2537L22.5975 26.1037L22.7025 24.6062L22.88 22.7812L22.91 22.5237ZM27.62 25.9112L27.5525 27.0787L27.5 28.3912L27.47 29.8412V31.3862L27.5 32.8362L27.55 34.1487L27.62 35.3162L28.66 34.7912L29.225 34.4962L30.445 33.8412L31.77 33.0937L33.075 32.3212L33.68 31.9512L34.79 31.2512L35.765 30.6137L34.785 29.9712L33.675 29.2687L32.435 28.5187C31.7799 28.1312 31.119 27.7536 30.4525 27.3862L29.2325 26.7312L28.1275 26.1637L27.62 25.9112Z" />
-          </svg>
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-20">
+          <div className="transform transition-transform duration-300 hover:scale-110">
+            <svg width="80" height="80" viewBox="0 0 61 61" fill="#D9FF85">
+              <path d="M30.5 5.89868C44.3075 5.89868 55.5 17.0912 55.5 30.8987C55.5 44.7062 44.3075 55.8987 30.5 55.8987C16.6925 55.8987 5.49998 44.7062 5.49998 30.8987C5.49998 17.0912 16.6925 5.89868 30.5 5.89868ZM30.5 10.8987C25.1957 10.8987 20.1086 13.0058 16.3579 16.7565C12.6071 20.5073 10.5 25.5944 10.5 30.8987C10.5 36.203 12.6071 41.2901 16.3579 45.0408C20.1086 48.7916 25.1957 50.8987 30.5 50.8987C35.8043 50.8987 40.8914 48.7916 44.6421 45.0408C48.3929 41.2901 50.5 36.203 50.5 30.8987C50.5 25.5944 48.3929 20.5073 44.6421 16.7565C40.8914 13.0058 35.8043 10.8987 30.5 10.8987ZM22.91 22.5237C22.9653 22.0638 23.127 21.6231 23.3823 21.2366C23.6377 20.8501 23.9796 20.5284 24.381 20.2971C24.7823 20.0659 25.2321 19.9314 25.6945 19.9043C26.157 19.8772 26.6194 19.9583 27.045 20.1412L27.8575 20.4987L28.9575 21.0037L29.83 21.4262L30.805 21.9162L31.88 22.4737L33.035 23.1012L34.27 23.8012L34.8925 24.1637L36.065 24.8687L37.135 25.5387L38.545 26.4587L39.705 27.2537L40.8425 28.0737L41.05 28.2262C42.6375 29.4187 42.65 31.7987 41.0525 32.9987L40.345 33.5212L39.3625 34.2112L38.1225 35.0487L37.155 35.6737L36.08 36.3487L34.8975 37.0562C34.6925 37.1779 34.4825 37.3004 34.2675 37.4237L33.0225 38.1287L31.8575 38.7612L30.7825 39.3212L29.8075 39.8112L28.545 40.4162L27.545 40.8687L27.04 41.0887C26.6146 41.2701 26.1527 41.35 25.6911 41.3221C25.2294 41.2941 24.7806 41.1591 24.3802 40.9277C23.9797 40.6963 23.6387 40.3749 23.3839 39.9889C23.1292 39.6029 22.9678 39.1629 22.9125 38.7037L22.7775 37.4562L22.7025 36.6212L22.5975 35.1237L22.5375 33.9737L22.4925 32.7087L22.4675 31.3362V29.8887L22.4925 28.5187L22.5375 27.2537L22.5975 26.1037L22.7025 24.6062L22.88 22.7812L22.91 22.5237ZM27.62 25.9112L27.5525 27.0787L27.5 28.3912L27.47 29.8412V31.3862L27.5 32.8362L27.55 34.1487L27.62 35.3162L28.66 34.7912L29.225 34.4962L30.445 33.8412L31.77 33.0937L33.075 32.3212L33.68 31.9512L34.79 31.2512L35.765 30.6137L34.785 29.9712L33.675 29.2687L32.435 28.5187C31.7799 28.1312 31.119 27.7536 30.4525 27.3862L29.2325 26.7312L28.1275 26.1637L27.62 25.9112Z" />
+            </svg>
+          </div>
         </div>
       )}
 
-      {/* Simple controls */}
-      {isActive && !showThumbnail && (
-        <div className="absolute bottom-2 left-2 right-2">
+      {/* Controles de hover para desktop */}
+      {isActive && !showThumbnail && isHovered && (
+        <div className="absolute bottom-4 left-4 right-4 flex items-center justify-between bg-black bg-opacity-70 rounded-lg p-2 transition-opacity duration-300">
           <button
-            onClick={togglePlay}
-            className="w-8 h-8 bg-black bg-opacity-50 text-white rounded-full flex items-center justify-center"
+            onClick={(e) => {
+              e.stopPropagation()
+              togglePlay()
+            }}
+            className="w-10 h-10 bg-white bg-opacity-20 text-white rounded-full flex items-center justify-center hover:bg-opacity-30 transition-all duration-200"
           >
-            <svg width="12" height="12" viewBox="0 0 24 24" fill="#D9FF85">
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="#D9FF85">
               {isPlaying ? (
                 <path d="M6 4h4v16H6V4zm8 0h4v16h-4V4z" />
               ) : (
@@ -129,13 +136,19 @@ const VideoPlayer: React.FC<{
               )}
             </svg>
           </button>
+
+          <div className="flex items-center space-x-2">
+            <span className="text-white text-xs">
+              {isPlaying ? 'Reproduzindo' : 'Pausado'}
+            </span>
+          </div>
         </div>
       )}
 
       {/* Title */}
       {video.title && (
-        <div className="absolute top-2 left-2 right-2">
-          <p className="text-white text-xs bg-black bg-opacity-50 px-2 py-1 rounded truncate">
+        <div className="absolute top-4 left-4 right-4">
+          <p className="text-white text-sm font-medium bg-black bg-opacity-70 px-3 py-2 rounded-lg truncate">
             {video.title}
           </p>
         </div>
@@ -144,13 +157,27 @@ const VideoPlayer: React.FC<{
   )
 }
 
-// Carousel super otimizado
+// Carousel otimizado para desktop
 const VideoCarouselModal: React.FC<VideoCarouselProps> = ({ videos }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
   const [isTransitioning, setIsTransitioning] = useState(false)
   const containerRef = useRef<HTMLDivElement>(null)
   const touchStart = useRef<number>(0)
   const touchEnd = useRef<number>(0)
+
+  // Navegação com teclado
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'ArrowLeft') {
+        navigate('prev')
+      } else if (e.key === 'ArrowRight') {
+        navigate('next')
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [])
 
   // Navegação com throttle
   const navigate = useCallback(
@@ -166,13 +193,12 @@ const VideoCarouselModal: React.FC<VideoCarouselProps> = ({ videos }) => {
         return (prev - 1 + videos.length) % videos.length
       })
 
-      // Usar setTimeout em vez de transitionend para ser mais previsível
-      setTimeout(() => setIsTransitioning(false), 250)
+      setTimeout(() => setIsTransitioning(false), 300)
     },
     [videos.length, isTransitioning]
   )
 
-  // Touch handlers minimalistas
+  // Touch handlers para mobile
   const handleTouchStart = useCallback((e: React.TouchEvent) => {
     touchStart.current = e.touches[0].clientX
   }, [])
@@ -180,7 +206,6 @@ const VideoCarouselModal: React.FC<VideoCarouselProps> = ({ videos }) => {
   const handleTouchEnd = useCallback(
     (e: React.TouchEvent) => {
       touchEnd.current = e.changedTouches[0].clientX
-
       const diff = touchStart.current - touchEnd.current
       const threshold = 50
 
@@ -195,55 +220,83 @@ const VideoCarouselModal: React.FC<VideoCarouselProps> = ({ videos }) => {
     [navigate]
   )
 
-  // Renderizar apenas 3 slides (prev, current, next)
+  // Renderizar slides com lógica mobile/desktop separada
   const renderSlides = () => {
     const slides = []
     const total = videos.length
+    const isMobile = window.innerWidth < 768
 
-    for (let i = -1; i <= 1; i++) {
-      const index = (currentIndex + i + total) % total
-      const video = videos[index]
-      const isActive = i === 0
+    if (isMobile) {
+      // Comportamento mobile original - apenas 3 slides (prev, current, next)
+      for (let i = -1; i <= 1; i++) {
+        const index = (currentIndex + i + total) % total
+        const video = videos[index]
+        const isActive = i === 0
 
-      slides.push(
-        <div
-          key={`${video.id}-${index}`}
-          className="absolute top-0 transition-all duration-300 ease-out"
-          style={{
-            left: '50%',
-            transform: `translateX(${-50 + i * 110}%) scale(${isActive ? 1 : 0.8})`,
-            opacity: isActive ? 1 : 0.6,
-            zIndex: isActive ? 20 : 10,
-            width: '180px',
-            height: '320px'
-          }}
-        >
-          <VideoPlayer video={video} isActive={isActive} />
-        </div>
-      )
+        slides.push(
+          <div
+            key={`${video.id}-${index}`}
+            className="absolute top-0 transition-all duration-300 ease-out"
+            style={{
+              left: '50%',
+              transform: `translateX(${-50 + i * 110}%) scale(${isActive ? 1 : 0.8})`,
+              opacity: isActive ? 1 : 0.6,
+              zIndex: isActive ? 20 : 10,
+              width: '180px',
+              height: '320px'
+            }}
+          >
+            <VideoPlayer video={video} isActive={isActive} />
+          </div>
+        )
+      }
+    } else {
+      // Comportamento desktop - múltiplos slides visíveis
+      for (let i = -2; i <= 2; i++) {
+        const index = (currentIndex + i + total) % total
+        const video = videos[index]
+        const isActive = i === 0
+
+        slides.push(
+          <div
+            key={`${video.id}-${index}`}
+            className="absolute top-0 transition-all duration-300 ease-out"
+            style={{
+              left: '50%',
+              transform: `translateX(${-50 + i * 85}%) scale(${isActive ? 1 : 0.8})`,
+              opacity: isActive ? 1 : 0.6,
+              zIndex: isActive ? 20 : 10 - Math.abs(i),
+              width: '280px',
+              height: '500px'
+            }}
+          >
+            <VideoPlayer video={video} isActive={isActive} />
+          </div>
+        )
+      }
     }
 
     return slides
   }
 
   return (
-    <div className="relative w-full max-w-sm mx-auto py-4">
+    <div className="relative w-full max-w-sm md:max-w-6xl mx-auto py-4 md:py-8">
       {/* Container principal */}
       <div
         ref={containerRef}
-        className="relative h-80 overflow-hidden"
+        className="relative h-80 md:h-[560px] overflow-hidden"
         onTouchStart={handleTouchStart}
         onTouchEnd={handleTouchEnd}
       >
-        {/* Navigation buttons */}
+        {/* Navigation buttons - apenas no desktop */}
         <button
           onClick={() => navigate('prev')}
           disabled={isTransitioning}
-          className="absolute left-2 top-1/2 -translate-y-1/2 z-30 w-8 h-8 bg-black bg-opacity-50 text-white rounded-full flex items-center justify-center disabled:opacity-30"
+          className="hidden md:flex absolute left-4 top-1/2 -translate-y-1/2 z-30 w-14 h-14 bg-black bg-opacity-60 text-white rounded-full items-center justify-center disabled:opacity-30 hover:bg-opacity-80 transition-all duration-200 hover:scale-110"
         >
           <svg
-            width="12"
-            height="12"
+            width="20"
+            height="20"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
@@ -256,11 +309,11 @@ const VideoCarouselModal: React.FC<VideoCarouselProps> = ({ videos }) => {
         <button
           onClick={() => navigate('next')}
           disabled={isTransitioning}
-          className="absolute right-2 top-1/2 -translate-y-1/2 z-30 w-8 h-8 bg-black bg-opacity-50 text-white rounded-full flex items-center justify-center disabled:opacity-30"
+          className="hidden md:flex absolute right-4 top-1/2 -translate-y-1/2 z-30 w-14 h-14 bg-black bg-opacity-60 text-white rounded-full items-center justify-center disabled:opacity-30 hover:bg-opacity-80 transition-all duration-200 hover:scale-110"
         >
           <svg
-            width="12"
-            height="12"
+            width="20"
+            height="20"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
@@ -272,6 +325,11 @@ const VideoCarouselModal: React.FC<VideoCarouselProps> = ({ videos }) => {
 
         {/* Slides */}
         {renderSlides()}
+      </div>
+
+      {/* Dica de navegação por teclado - só no desktop */}
+      <div className="hidden md:block text-center mt-4 text-gray-400 text-sm">
+        Use as setas ← → do teclado para navegar
       </div>
     </div>
   )
